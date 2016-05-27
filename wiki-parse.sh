7a7d8d2f0991cf44b956c34633c762ff82079b8b
sheacd7 <cds5g@virginia.edu>
2016-05-27 04:51:41 -0400

    parse all infobox content
    parse infobox content by category
    grep start and end lines of infobox of each page outside loop (for
    speed)

--- wiki-parse.sh
+++ wiki-parse.sh
@@ -11,9 +11,8 @@
 scriptname=$(basename $0)
 function usage {
   echo "Usage: $scriptname"
-  echo "Parse wikipedia ."
+  echo "Parse wikipedia infobox and return info."
   echo "  -i, --list          list of wiki pages"
-  echo "  -d, --disambiguate  additional term for all subjects"
   echo "  -h, --help          display help"
 }
 
@@ -22,102 +21,132 @@ while [[ $# > 0 ]]; do
   arg="$1"
   case $arg in
     -i|--list)         shift; LIST="$1" ;;
-    -d|--disambiguate) shift; DISAM="$1" ;;
     -h|--help)         usage; exit ;;        # print help
-    *) PAGE="$1" ;; 
+#    *) PAGE="$1" ;; 
   esac
   shift
 done
 
 # define file input/output
-#ROOTDIR="/cygdrive/c/Users/sheacd/GitHub/WebUtils"
 ROOTDIR="$(dirname $0)"
-#printf '%s\n' "${ROOTDIR}"
-topic=""
 
-# # define input
-if [[ ! -z "${PAGE}" ]]; then 
-  pages[0]="${PAGE}"
-elif [[ ! -z "${LIST}" ]]; then
-  mapfile -t pages < "${LIST}"
+# define input
+if [[ ! -z "${LIST}" ]]; then
+  pages="${LIST}"
+#  mapfile -t pages < "${LIST}"
 else
   echo "Error: No input pages. Exiting."
   exit
 fi
-# if [[ ! -z "${DISAM}" ]]; then
-#   topic="${DISAM}"
-# fi
-# 
-# # params
-# DELAY=5
 
-for page in "${pages[@]}"; do
 
-  printf '\n%s' "${page}"
+mapfile -t infobox_starts < <(
+  grep -nm 1 -e '<table class="infobox vcard plainlist"' \
+             -e '<table class="infobox biography vcard"' \
+  $(cat "${pages}") | \
+  cut -f 1,2 -d ':' )
 
-  # parse page for background information (infobox)
-  infobox_start=""
-  infobox_end=""
-  infobox_start=$( grep -nm 1 '<table class="infobox vcard plainlist"' "${page}" | \
-                   cut -f 1 -d ':' )
-  if [[ -z "${infobox_start}" ]]; then
-    infobox_start=$( grep -nm 1 '<table class="infobox biography vcard"' "${page}" | \
-                     cut -f 1 -d ':' )
-    [[ -z "${infobox_start}" ]] && continue
+mapfile -t infobox_ends < <(
+  grep -n '</table>' \
+  $(cat "${pages}") | \
+  cut -f 1,2 -d ':' )
+
+# split into page:line
+# use page as key
+# use line as value
+declare -A starts 
+for val in "${infobox_starts[@]}"; do
+  starts["${val%%:*}"]="${val##*:}"
+done
+
+declare -A ends 
+# save first end line that is > start line
+for val in "${infobox_ends[@]}"; do 
+  page="${val%%:*}"
+  line="${val##*:}"
+  if [[ "${ends["${page}"]}" == "" && ${line} -gt ${starts["${page}"]} ]]; then
+    ends["${page}"]=${line}
   fi
+done
 
-  mapfile -t table_ends < <( grep -n '</table>' "${page}" | \
-                             cut -f 1 -d ':' )
-  for end in "${table_ends[@]}"; do 
-    [[ $end -gt $infobox_start ]] && infobox_end=$end && break
-  done
-  [[ -z "${infobox_end}"   ]] && continue 
 
-  head_line=$(( infobox_end - 1 ))
-  tail_line=$(( infobox_end - infobox_start ))
+for page in "${!ends[@]}"; do
+  printf '%s\n' "${page}"
 
-  mapfile -t infobox_content < <( head -${head_line} ${page} | \
-                                  tail -${tail_line} | \
-                                  sed 's,<[^>]*>,,g;s,\[[^\]]*\],,g' | \
-                                  sed '/^$/d' )
+  head_line=$(( ends["${page}"] - 1 ))
+  tail_line=$(( ends["${page}"] - starts["${page}"] ))
 
-  # parse infobox for genres
-  genre_start=""
-  genre_end=""
+  mapfile -t infobox_content < <( 
+    head -${head_line} "${page}" | \
+    tail -${tail_line} | \
+    sed -e 's,<th scope="row">,CATEGORY,g' \
+        -e 's,<[^>]*>,,g' \
+        -e 's,\[[^\]]*\],,g' \
+        -e 's,&#160;, ,g' | \
+    sed '/^$/d' )
+
+#   printf '%s\n' "${infobox_content[@]}"
+  declare -A category_limits
+  category=""
   for line in "${!infobox_content[@]}"; do 
-    case "${infobox_content[$line]}" in 
-      'Genres')       
-        genre_start=${line}; continue;;
-      'Years active'|'Occupation(s)'|'Instruments'|'Website'|'Labels') 
-        [[ -z "${genre_start}" ]] && continue;
-        genre_end=${line};   break;;
+    case "${infobox_content[$line]}" in
+      CATEGORY*)
+        [[ ! -z "${category}" ]] && category_limits["${category}"]+=",${line}"
+        category="${infobox_content[$line]#CATEGORY}"
+        infobox_content[$line]="${category}"
+        category_limits["${category}"]="${line}" ;;
     esac
   done
-  if [[ ! -z "${genre_start}" && ! -z "${genre_end}" ]]; then
-    printf ', %s' "${infobox_content[@]:$((genre_start + 1)):$((genre_end - genre_start - 1))}"
-  fi
+  [[ ! -z "${category}" ]] && category_limits["${category}"]+=",${#infobox_content[@]}"
+  for category in "${!category_limits[@]}"; do 
+    start="${category_limits[${category}]%%,*}"
+    end="${category_limits[${category}]##*,}"
+    printf '%s' "${category}"
+    printf ', %s' "${infobox_content[@]:$((start + 1)):$((end - start - 1))}"
+    printf '\n'
+  done
+  unset category_limits
 done
 
 # scratch ======================================================================
 
-# function raw_url_encode() {
-#   local string="${1}"
-#   local strlen=${#string}
-#   local encoded=""
-#   local pos c o
-# 
-#   for (( pos=0 ; pos<strlen ; pos++ )); do
-#      c=${string:$pos:1}
-#      case "$c" in
-#         [-_.~a-zA-Z0-9] ) o="${c}" ;;
-#         * ) printf -v o '%%%02x' "'$c" ;;
-#      esac
-#      encoded+="${o}"
-#   done
-#   url_enc_string="${encoded}"
-# }
-# 
-# function raw_url_decode() {
-#   # replace hex chars with ascii equivalents
-#   printf -v url_dec_string '%b' "${1//%/\\x}"
-# }
+#  # parse page for background information (infobox)
+#  infobox_start=""
+#  infobox_end=""
+#  infobox_start=$( 
+#    < "${page}" \
+#    grep -nm 1 -e '<table class="infobox vcard plainlist"'
+#               -e '<table class="infobox biography vcard"' | \
+#    cut -f 1 -d ':' )
+#  [[ -z "${infobox_start}" ]] && continue
+#  
+#
+#  mapfile -t table_ends < <( grep -n '</table>' "${page}" | \
+#                             cut -f 1 -d ':' )
+#  for end in "${table_ends[@]}"; do 
+#    [[ $end -gt $infobox_start ]] && infobox_end=$end && break
+#  done
+#  [[ -z "${infobox_end}"   ]] && continue 
+
+#  head_line=$(( infobox_end - 1 ))
+#  tail_line=$(( infobox_end - infobox_start ))
+
+#  mapfile -t infobox_content < <( head -${head_line} ${page} | \
+#                                  tail -${tail_line} | \
+#                                  sed 's,<[^>]*>,,g;s,\[[^\]]*\],,g' | \
+#                                  sed '/^$/d' )
+# parse infobox for genres
+#  genre_start=""
+#  genre_end=""
+#  for line in "${!infobox_content[@]}"; do 
+#    case "${infobox_content[$line]}" in 
+#      'Genres')       
+#        genre_start=${line}; continue;;
+#      'Years active'|'Occupation(s)'|'Instruments'|'Website'|'Labels') 
+#        [[ -z "${genre_start}" ]] && continue;
+#        genre_end=${line};   break;;
+#    esac
+#  done
+#  if [[ ! -z "${genre_start}" && ! -z "${genre_end}" ]]; then
+#    printf ', %s' "${infobox_content[@]:$((genre_start + 1)):$((genre_end - genre_start - 1))}"
+#  fi
